c3b48792c6e023afdfca973396cfe986f1d44f2a
AndreySemin <andreu_semin@mail.ru>
2025-08-25 20:56:47 +0300

    Update Animals.java

--- src/main/java/Lesson_4/Animals.java
+++ src/main/java/Lesson_4/Animals.java
@@ -1,1 +1,12 @@
+  protected String name;
+    Animals(){countAnimal++;}
 
+    public Animals(String name){
+        this.name = name;
+        countAnimal ++;
+    }
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+
+    public static int getCountAnimal(){return countAnimal;}
