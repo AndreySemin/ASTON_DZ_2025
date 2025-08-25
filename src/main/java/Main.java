9e9c54b2d28042746ca1adf7e43a90c72916c9a8
Dom <andreu_semin@mail.ru>
2025-08-25 18:47:04 +0300

    Lesson_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import lesson_3.Park;
+import lesson_3.Product;
 import lesson_4.Animals;
 import lesson_4.Bowl;
 import lesson_4.Cat;
@@ -6,6 +8,8 @@ import lesson_4_tack_2.Circle;
 import lesson_4_tack_2.Rectangle;
 import lesson_4_tack_2.Shapes;
 import lesson_4_tack_2.Triangle;
+
+import java.util.Arrays;
 
 public class Main {
 
@@ -49,55 +53,41 @@ public class Main {
         }
 
         System.out.println("Фигуры");
-        Shapes circle = new Circle(10,"red", "blue");
+        Shapes circle = new Circle(10, "red", "blue");
         System.out.println("Круг");
         circle.printSpecifications();
 
-        Shapes rectangle = new Rectangle(10,15,"white", "black");
+        Shapes rectangle = new Rectangle(10, 15, "white", "black");
         System.out.println("Прямоугольник");
         rectangle.printSpecifications();
 
-        Shapes triangle = new Triangle(10,10,10, "Green", "Grey");
+        Shapes triangle = new Triangle(10, 10, 10, "Green", "Grey");
         System.out.println("Треугольник");
         triangle.printSpecifications();
+
+        Lesson_3();
 
     }
 
 
 
 
+    public static void Lesson_3() {
+            Product[] product = new Product[5];
+            product[0] = new Product("Redmi_14_Pro ", "10-05-2024", "Huavei ", "China ", 20000, "boked ");
+            product[1] = new Product("Redmi_13_Pro ", "10-01-2024", "Huavei ", "China ", 18000, "boked ");
+            product[2] = new Product("Redmi_12_Pro ", "10-12-2023", "Huavei ", "China ", 16000, "boked ");
+            product[3] = new Product("Redmi_11_Pro ", "10-01-2023", "Huavei ", "China ", 15000, "boked ");
+            product[4] = new Product("Redmi_10_Pro ", "10-03-2022", "Huavei ", "China ", 13000, "boked ");
+
+            Arrays.stream(product).forEach(Product::printInfo);
+
+            Park park = new Park("Планета", "ул. Николая Островского 148");
+            Park.Attraction bigwheel = park.new Attraction("Большое Колесо", "C 9:00 - 20:00", "150 rub");
+            bigwheel.printInfo();
+
 
         }
-
-
-
-
-
-
-
-
-/*import lesson_3.Park;
-import lesson_3.Product;
-
-import java.util.Arrays;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Product[] product = new Product[5];
-        product[0] = new Product("Redmi_14_Pro ", "10-05-2024"  , "Huavei ", "China " , 20000  ,"boked ");
-        product[1] = new Product("Redmi_13_Pro ", "10-01-2024"  , "Huavei ", "China " , 18000  ,"boked ");
-        product[2] = new Product("Redmi_12_Pro ", "10-12-2023"  , "Huavei ", "China " , 16000  ,"boked ");
-        product[3] = new Product("Redmi_11_Pro ", "10-01-2023"  , "Huavei ", "China " , 15000  ,"boked ");
-        product[4] = new Product("Redmi_10_Pro ", "10-03-2022"  , "Huavei ", "China " , 13000  ,"boked ");
-
-        Arrays.stream(product).forEach(Product::printInfo);
-
-        Park park = new Park("Планета", "ул. Николая Островского 148");
-        Park.Attraction bigwheel = park.new Attraction("Большое Колесо", "C 9:00 - 20:00", "150 rub");
-        bigwheel.printInfo();
-
-
     }
 
 
